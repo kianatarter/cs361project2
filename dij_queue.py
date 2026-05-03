74cb76731ab40f2f16b1777e786ff2e88cc091ac
Kiana Tarter <kctarter@gmail.com>
2026-05-03 13:58:43 -0600

    Added priority queue implementation

--- dij_queue.py
+++ dij_queue.py
@@ -5,5 +5,31 @@ Kiana Tarter and Neveah Martinez
 
 '''
 
-def priority_queue_dij(): ...
+import heapq
+import sys
 
+def priority_queue_dij(adj, src): 
+    V = len(adj)
+
+    queue = []
+
+    # set all elements of solution to infinity initially
+    dist = [sys.maxsize] * V
+    
+    # set the path from the source to itself to 0
+    dist[src] = 0
+
+    heapq.heappush(queue, (0,src))
+
+    while queue: 
+        d, u = heapq.heappop(queue)
+
+        if (d > dist[u]):
+            continue
+
+        for vertex, cost in adj[u]:
+
+            if dist[u] + cost < dist[vertex]:
+                dist[vertex] = dist[u] + cost
+                heapq.heappush(queue, (dist[vertex], cost))
+    return dist
